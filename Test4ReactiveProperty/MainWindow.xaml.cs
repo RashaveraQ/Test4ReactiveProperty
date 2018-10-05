2167f4dcccc73d77cd2c48e3c0ed7422f81a6e96
RashaveraQ <so_dandy@yahoo.co.jp>
2018-10-06 00:20:54 +0900

    add PosX and PosY as ReactiveProperty<double> in MainWindow class.

--- Test4ReactiveProperty/MainWindow.xaml.cs
+++ Test4ReactiveProperty/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Reactive.Bindings;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Test4ReactiveProperty
 {
@@ -20,9 +8,13 @@ namespace Test4ReactiveProperty
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ReactiveProperty<double> PosX { get; } = new ReactiveProperty<double>(100);
+        public ReactiveProperty<double> PosY { get; } = new ReactiveProperty<double>(20);
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
     }
 }
