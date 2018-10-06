9f5d96bf24edb6dea91d947d74aeaba84eb5de56
RashaveraQ <so_dandy@yahoo.co.jp>
2018-10-06 10:18:31 +0900

    add another Red sphere.

--- Test4ReactiveProperty/MainWindow.xaml.cs
+++ Test4ReactiveProperty/MainWindow.xaml.cs
@@ -10,22 +10,29 @@ namespace Test4ReactiveProperty
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ReactiveProperty<Point> Pos { get; } = new ReactiveProperty<Point>();
+        public ReactiveProperty<Point>[] Points { get; } = {
+            new ReactiveProperty<Point>(),
+            new ReactiveProperty<Point>()
+        };
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
 
-            Observable.Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(1))
-                .Select(i => i * 2 * Math.PI / 360)
-                .Subscribe( th =>
-                    {
-                        Point c = new Point(100, 100);
-                        const double r = 100;
-                        Pos.Value = new Point(c.X + r * Math.Cos(th), c.Y + r * Math.Sin(th));
-                    }
-                );
+            Point c = new Point(100, 100);
+            const double r = 100;
+
+            var source = Observable
+                .Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(1))
+                .Select(i => i * 2 * Math.PI / 360);
+           
+            source
+                .Subscribe( th => Points[0].Value = new Point(c.X + r * Math.Cos(th), c.Y + r * Math.Sin(th)));
+
+            source
+                .Select(x => 2 * x)
+                .Subscribe(th => Points[1].Value = new Point(c.X + r * Math.Cos(th), c.Y + r * Math.Sin(th)));
         }
     }
 }
