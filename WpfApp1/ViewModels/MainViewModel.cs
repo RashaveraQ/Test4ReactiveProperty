9516feb84e56c202fbc0c4666aaa58f4be23dbb2
RashaveraQ <so_dandy@yahoo.co.jp>
2018-10-27 19:38:21 +0900

    Binding properties of IsSelected and IsChecked with ViewModel.

--- WpfApp1/ViewModels/MainViewModel.cs
+++ WpfApp1/ViewModels/MainViewModel.cs
@@ -5,14 +5,15 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WpfApp1.Models;
 
 namespace WpfApp1.ViewModels
 {
     public class Person4View : Person
     {
-        public ReactiveProperty<bool> IsSelected { get; set; } = new ReactiveProperty<bool>(false);
-        public ReactiveProperty<bool> IsChecked { get; set; } = new ReactiveProperty<bool>(false);
+        public ReactiveProperty<bool> IsSelected { get; set; }
+        public ReactiveProperty<bool> IsChecked { get; set; }
 
         public Person4View(Person person) : base(person) {}
     }
@@ -25,7 +26,12 @@ namespace WpfApp1.ViewModels
 
         public MainViewModel()
         {
-            People = _model.People.ToReadOnlyReactiveCollection(x => new Person4View(x));
+            var r = new System.Random(Environment.TickCount);
+
+            People = _model.People.ToReadOnlyReactiveCollection(x => new Person4View(x) {
+                IsSelected = new ReactiveProperty<bool>(r.Next() % 2 == 0),
+                IsChecked = new ReactiveProperty<bool>(r.Next() % 2 == 0),
+            });
         }
     }
 }
