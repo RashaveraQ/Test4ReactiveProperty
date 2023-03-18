609d1648d022c584f631376a8bc33f0e10d9bc9b
RashaveraQ <so_dandy@yahoo.co.jp>
2023-03-18 15:47:49 +0900

    ListBox Item の Click コマンドの実証プロジェクトを追加と、VS2022 に更新。

---
+++ Test4ListBoxItemClickCommand/Models/Offering.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1.Models
+{
+    public class Offering : INotifyPropertyChanged
+    {
+        public string? Title { get; set; }
+
+        bool isAlreadyRead;
+        public bool IsAlreadyRead
+        {
+            get => isAlreadyRead;
+            set {
+                isAlreadyRead = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? caller = null) =>
+            PropertyChanged?.Invoke(this, new (caller));
+    }
+}
---
+++ Test4ListBoxItemClickCommand/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Reactive.Bindings;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    public class MainViewModel
+    {
+        public ReactiveCollection<Offering> Offerings { get; set; }
+
+        public ReactiveCommand<Offering> ClickCommand { get; set; }
+
+        public MainViewModel()
+        {
+            Offerings = new ReactiveCollection<Offering>();
+
+            Offerings.AddRangeOnScheduler(new Offering[] {
+                new Offering() { Title = "hoge", IsAlreadyRead = false},
+                new Offering() { Title = "fuga", IsAlreadyRead = false},
+                new Offering() { Title = "piyo", IsAlreadyRead = true},
+                new Offering() { Title = "hoge2", IsAlreadyRead = false},
+            });
+
+            ClickCommand = new ReactiveCommand<Offering>()
+                .WithSubscribe( offering => offering.IsAlreadyRead = true);
+        }
+    }
+}
